b0e47d6fb1e58f97753bd738ac8754f356fc3283
AnSu-2a2 <as21yd@brocku.ca>
2024-11-24 00:18:29 -0500

    Edited Lab 10

---
+++ Lab_10/test.js
@@ -0,0 +1,17 @@
+function start(){
+    var one=Number(document.getElementById("num1").value);
+    var two=Number(document.getElementById("num2").value);
+}
+    var three=one+two;
+    var four=one-two;
+    var five=one*two;
+    var six=one/two;
+
+if(id="add"){document.getElementById("output").innerHTML=three;}
+
+else if(id="subtract"){document.getElementById("output").innerHTML=four;}
+
+else if(id="multiply"){document.getElementById("output").innerHTML=five;}
+
+else if(id="divide"){document.getElementById("output").innerHTML=six;}
+
